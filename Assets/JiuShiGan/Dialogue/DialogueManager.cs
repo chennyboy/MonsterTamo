4e0ce0262692fd5c2a6304b388ac7e93ce9b084a
Wenchao Chen <chen.wenchao.dev@gmail.com>
2020-03-02 11:19:43 +0800

    init commit

---
+++ Assets/JiuShiGan/Dialogue/DialogueManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public GameObject panel;
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        panel.SetActive(false);
+
+    }
+
+    public void StartDialogue(Dialogue dialogue) {
+        Debug.Log("starting " + dialogue.name);
+        panel.SetActive(true);
+
+        sentences.Clear();
+        nameText.text = dialogue.name;
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray()) {
+            dialogueText.text += letter;
+            yield return null; 
+        }
+    }
+
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+        Debug.Log(sentence);
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        
+    }
+    public void EndDialogue()
+    {
+        panel.SetActive(false);
+    }
+}
